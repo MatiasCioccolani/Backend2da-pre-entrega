05288d340ff69d831007a523953ecbb7759bf380
MatiasCioccolani <117781520+MatiasCioccolani@users.noreply.github.com>
2023-06-15 22:50:14 -0300

    Delete src/routes directory

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const productsApi = require('../components/products')
-const cartsApi = require('../components/cart')
-
-module.exports = (app) => {
-  productsApi(app)
-  cartsApi(app)
-  app.get('/', (req, res) => res.send('Hello world!'));
-}
